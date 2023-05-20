326e878b30433797763c1508a05627512466909c
Henxu <henxu777@gmail.com>
2023-05-20 18:15:36 +0100

    complete add/delete/update function

--- src/main/java/com/itheima/controller/DeptController.java
+++ src/main/java/com/itheima/controller/DeptController.java
@@ -7,14 +7,14 @@ import lombok.extern.slf4j.Slf4j;
 //import org.slf4j.Logger;
 //import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 //import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @Slf4j
+@RequestMapping("/depts")
 @RestController
 public class DeptController {
 
@@ -23,12 +23,44 @@ public class DeptController {
     private DeptService deptService;
 
 //    @RequestMapping(value = "/depts", method = RequestMethod.GET)
-    @GetMapping("/depts")
+    @GetMapping
     public Result list(){
         log.info("Query for all department information");
 
         List<Dept> deptList = deptService.list();
         return Result.success(deptList);
+    }
+
+    @DeleteMapping("/{id}")
+    public Result delete(@PathVariable Integer id){
+        log.info("Delete the selected department by id: {}", id);
+
+        deptService.delete(id);
+        return Result.success();
+    }
+
+    @PostMapping
+    public Result addDept(@RequestBody Dept dept){
+        log.info("Add a new department: {}", dept);
+        deptService.addDept(dept);
+
+        return Result.success();
+    }
+
+    @GetMapping("/{id}")
+    public Result getById(@PathVariable Integer id){
+        log.info("Get the department name by id: {}", id);
+
+        Dept dept = deptService.getById(id);
+        return Result.success(dept);
+    }
+
+    @PutMapping
+    public Result updateDept(@RequestBody Dept dept){
+        log.info("update department name: {}", dept);
+        deptService.updateDept(dept);
+
+        return Result.success();
     }
 
 
--- src/main/java/com/itheima/service/DeptService.java
+++ src/main/java/com/itheima/service/DeptService.java
@@ -5,5 +5,14 @@ import com.itheima.pojo.Dept;
 import java.util.List;
 
 public interface DeptService {
+
     List<Dept> list();
+
+    void delete(Integer id);
+
+    void addDept(Dept dept);
+
+    Dept getById(Integer id);
+
+    void updateDept(Dept dept);
 }
